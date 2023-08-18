69387e2a95e28157bc8908baf7a7ecd3d5633a8d
abiiralbhattarai <abiiralbhattarai@gmail.com>
2023-08-18 22:50:56 +0545

    Eth contract

---
+++ src/bmxETH.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.18;
+
+import {ERC20PermitPermissionedMint} from "./ERC20/ERC20PermitPermissionedMint.sol";
+
+contract frxETH is ERC20PermitPermissionedMint {
+    /* ========== CONSTRUCTOR ========== */
+    constructor(
+        address _creator_address,
+        address _timelock_address
+    )
+        ERC20PermitPermissionedMint(
+            _creator_address,
+            _timelock_address,
+            "BMX Ether",
+            "bmxETH"
+        )
+    {}
+}
